09c405403e4e5a9032ea5c5a8dfb4858a28c91ad
Imteyaz Qamer Raja ADM <irajaadm@corp.local>
2020-12-28 22:09:04 +0530

    jest test case - sec10

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,45 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); // https
-app.use(json());
-
-app.use(
-  cookieSession({
-    signed: false, // no encryption
-    secure: true // https
-  })
-);
-
-app.use(signinRouter);
-app.use(currentUserRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-/*
-without express-async-errors pkg
-app.all('*', async (req, res, next) => {
-  next(new NotFoundError());
-});
-*/
-
-app.use(errorHandler);
-
+import { app } from './app';
 const start = async () => {
   if(!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
